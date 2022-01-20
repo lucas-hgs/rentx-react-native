8758e92ec0dfe60ea4ce34488e80bdeb055ba31b
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-01-20 00:45:33 -0300

    Added Cars to be Listed on MyCars Screen

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from 'react';
+import { StatusBar, FlatList } from 'react-native';
+import { useTheme } from 'styled-components';
 
+import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
+
+import { Car } from '../../components/Car';
 import { CarDTO } from '../../dtos/CarDTO';
 import { api } from '../../services/api';
 
+import { BackButton } from '../../components/BackButton';
+
 import {
-  Container
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
 } from './styles';
 
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
+
 export function MyCars(){
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const theme = useTheme();
+  const { goBack }:NavigationProp<ParamListBase> = useNavigation();
+
+
+  function handleGoBack() {
+    goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -28,7 +56,42 @@ export function MyCars(){
 
   return (
     <Container>
+      <Header>
+        <StatusBar 
+          barStyle='light-content'
+          translucent
+          backgroundColor='transparent'
+        />
+        <BackButton 
+          onPress={handleGoBack}
+          color={theme.colors.shape}
+        />
 
+        <Title>
+          Seus agendamentos,{'\n'}
+          estão aqui.
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>05</AppointmentsQuantity>
+        </Appointments>
+
+        <FlatList 
+          data={cars}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <Car data={item.car} />
+          )}
+        />
+      </Content>
     </Container>
   );
 }
