c0d6b3801098bed58c4d0631a24edcfcb2d94c7c
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-01-28 19:07:48 -0300

    Criado tela de Login

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -13,9 +13,10 @@ interface Props {
   onPress: () => void;
   enabled?: boolean;
   loading?: boolean;
+  light?: boolean;
 }
 
-export function Button({title, color, enabled = true, loading = false, ...rest}: Props){
+export function Button({title, color, enabled = true, loading = false, light = false, ...rest}: Props){
   const theme = useTheme();
 
   return (
@@ -27,7 +28,7 @@ export function Button({title, color, enabled = true, loading = false, ...rest}:
     >
       {
         loading ? <ActivityIndicator color={theme.colors.shape} /> :
-        <Title>{title}</Title>
+        <Title light={light}>{title}</Title>
       }
     </Container>
   );
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -9,6 +9,7 @@ import { SchedulingComplete } from '../screens/SchedulingComplete';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
+import { SignIn } from '../screens/SignIn';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -16,11 +17,11 @@ export function StackRoutes() {
   return (
     <Navigator 
       screenOptions={{headerShown: false}}
-      initialRouteName='Splash'
+      initialRouteName='SignIn'
     >
       <Screen 
-        name='Splash'
-        component={Splash}
+        name='SignIn'
+        component={SignIn}
       />
       <Screen 
         name='Home'
