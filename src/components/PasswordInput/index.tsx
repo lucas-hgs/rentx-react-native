3346c96c958945266acf08c60f33e24d4edaca90
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-01-31 18:58:35 -0300

    Adicionado acessibilidades aos inputs

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -11,14 +11,18 @@ import {
 } from './styles';
 
 interface InputProps extends TextInputProps {
-  iconName: React.ComponentProps<typeof Feather>['name']
+  iconName: React.ComponentProps<typeof Feather>['name'];
+  value?: string;
 }
 
 export function PasswordInput({ 
   iconName,
+  value,
   ...rest
 }: InputProps){
   const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
   const theme = useTheme();
 
@@ -27,17 +31,28 @@ export function PasswordInput({
     console.log(isPasswordVisible);
   }
 
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }
+
   return (
-    <Container>
+    <Container isFocused={isFocused}>
       <IconContainer>
         <Feather 
           name={iconName}
           size={24}
-          color={theme.colors.text_detail}
+          color={(isFocused || isFilled) ? theme.colors.main : theme.colors.text_detail}
         />
       </IconContainer>
 
       <InputText
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         secureTextEntry={isPasswordVisible} 
         {...rest} 
       />
