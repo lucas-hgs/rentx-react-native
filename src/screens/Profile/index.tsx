9b9471cfbe4db1b66cbd64120665467737f4b28f
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-02-06 20:52:06 -0300

    Criado Header do Profile

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons'
+
+import { BackButton } from '../../components/BackButton';
+
+import {
+  Container,
+  Header,
+  HeaderTop,
+  HeaderTitle,
+  LogoutButton,
+  PhotoContainer,
+  Photo,
+  PhotoButton
+} from './styles';
+
+export function Profile(){
+  const theme = useTheme();
+  const { goBack }:NavigationProp<ParamListBase> = useNavigation();
+
+  function handleBack() {
+    goBack();
+  }
+
+  function handleSignOut() {
+
+  }
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTop>
+          <BackButton color={theme.colors.shape} onPress={handleBack} />
+          <HeaderTitle>Editar Perfil</HeaderTitle>
+          <LogoutButton onPress={handleSignOut}>
+            <Feather name='power' size={24} color={theme.colors.shape} />
+          </LogoutButton>
+        </HeaderTop>
+
+        <PhotoContainer>
+          <Photo source={{ uri: 'https://github.com/lucas-hgs.png' }} />
+          <PhotoButton onPress={() => {}}>
+            <Feather name='camera' size={24} color={theme.colors.shape} />
+          </PhotoButton>
+        </PhotoContainer>
+      </Header>
+    </Container>
+  );
+}
