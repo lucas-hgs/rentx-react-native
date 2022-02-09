ebf6df9fb1516c124c41117f348cd4bce93ecd9e
Lucas Henrique Gonçalves da Silveira <lucas.hgs03@gmail.com>
2022-02-09 18:51:15 -0300

    Adicionado config no Main Activity para funcionar o Gesture Handler

--- android/app/src/main/java/com/rentx/MainActivity.java
+++ android/app/src/main/java/com/rentx/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView; // Linha que deve ser adicionada
 
 import expo.modules.ReactActivityDelegateWrapper;
 
@@ -29,11 +30,16 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this,
-      new ReactActivityDelegate(this, getMainComponentName())
-    );
-  }
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegateWrapper(this,
+        new ReactActivityDelegate(this, getMainComponentName()) {  
+          @Override // Linha que deve ser adicionada
+            protected ReactRootView createRootView() { // Linha que deve ser adicionada
+            return new RNGestureHandlerEnabledRootView(MainActivity.this); // Linha que deve ser adicionada
+          }
+        }
+      );
+    }
 
   /**
    * Align the back button behavior with Android S
